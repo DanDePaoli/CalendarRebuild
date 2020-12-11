8648b701bb6aea6d3eaa82b3364fe0188bf05401
Daniel DePaoli <dannydepaoli0@gmail.com>
2020-12-11 12:32:42 -0800

    Server construction

---
+++ client/src/app.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import axios from 'axios';
+
+
+
+class App extends React.Component {
+constructor(props){
+super(props);
+this.state = {}
+}
+
+render () {
+
+
+  return (
+<div>
+<h1>React is working!</h1>
+</div>
+  );
+
+}
+
+
+}
+
+export default App;
